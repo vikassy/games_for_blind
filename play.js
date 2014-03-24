f505536d18b5ecd99d1294fa2c2be4ef0b54ffaa
Gaurav yadav <gauravyadav2099@gmail.com>
2014-03-24 08:58:35 +0530

    Done with complexity1

--- play.js
+++ play.js
@@ -15,7 +15,7 @@ function play()
 	// {
 		// console.log("car");
 		//Then it is car
-		decide_direction(car)
+		decide_direction(car) //in variable .js
 	// }
 }
 
@@ -28,8 +28,12 @@ function play()
 
 window.onload = function()
 {
+window
 	speak.play('You are on left side initially. Avoid collision with other cars which are coming towards your side.');
+	
 	play_again = setInterval(play, 8000); //16 
+	
+	
 	score.start();
 }
 // car.play_right()
